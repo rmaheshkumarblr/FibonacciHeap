4c5df29258c864ca8522ae84a9f97cb19911bcf9
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-03-27 17:23:25 -0600

    FibonacciHeap Union two Fibonacci Heaps

--- FibonacciHeap.py
+++ FibonacciHeap.py
@@ -18,13 +18,13 @@ class FibonacciHeap:
             self.mark = False
 
     # Inserting a new node into a Fibonacci Heap
-    def Insert(self,x):
+    def insert(self,x):
         # Creating a Fibonacci Node ( A Fibonacci Heap )
         n = self.Node(x)
         # Making the two pointers point to itself
         n.left = n.right = n
         # Concatenate the root list containing x with root list H
-        self.mergeWithRootList(n)
+        self.mergeNewNodeWithRootList(n)
         if ( self.minNode is None or n.x < self.minNode.x ):
             self.minNode = n
         self.totalNodes += 1
@@ -33,8 +33,36 @@ class FibonacciHeap:
     def findMinNode(self):
         return self.minNode
 
+    # Uniting two Fibonacci Heaps
+    def union(self,h2):
+        # Creating a new Fibonacci Heap
+        h1 = FibonacciHeap()
+        # Assigning the properties of the current Fibonacci Heap to the newly created Fibonacci Heap
+        h1.rootList = self.rootList
+        h1.minNode = self.minNode
+        # Concatenate the root list of h2 with the root list of h1
+        self.mergeRootListWithExistingRootList(h1,h2)
+        # Update minNode if required
+        if ( h1.minNode is None or (h2.minNode is not None and h2.minNode.x < h1.minNode.x) ):
+            h1.minNode = h2.minNode
+        # Update total Nodes
+        h1.totalNodes = self.totalNodes + h2.totalNodes
+        # return the Fibonacci Heap
+        return h1
 
-    def mergeWithRootList(self,node):
+
+
+
+    def mergeRootListWithExistingRootList(self,h1,h2):
+        tempStorage = h2.rootList.left
+        h2.rootList.left = h1.rootList.left
+        h1.rootList.left.right = h2.rootList
+        h1.rootList.left = tempStorage
+        h1.rootList.left.right = h1.rootList
+
+
+
+    def mergeNewNodeWithRootList(self,node):
         if self.rootList is None:
             self.rootList = node
         else:
